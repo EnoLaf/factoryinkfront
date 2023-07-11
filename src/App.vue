4f575e32c82f37c48870811376f26031bb538352
Enora Lafforgue <enora.lafforgue@gmail.com>
2023-07-11 18:48:34 +0200

    add logo and realise navbar

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,32 @@ export default {
 }
 </script>
 
+<style>
+/* Fonts import */
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;500;600&display=swap');
+
+/* CSS Reset */
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body{
+  /* colors */
+  --black : #14110E;
+  --light-grey: #EFEFF1;
+  --light-grey-70: rgba(239,239,241,0.7);
+  --light-grey-10: rgba(239,239,241,0.1);
+  --logo-color: #E65500;
+  --bronze: #CC6633;
+  --bronze-50: rgba(204,102,51,0.5);
+  --bronze-30: rgba(204,102,51,0.3);
+
+  background-color: var(--black);
+  color: var(--light-grey);
+}
+</style>
+
 
--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,27 +1,42 @@
 <!-- src/components/NavbarComponent.vue -->
 <template>
-    <nav>
-        <ul >
+    <header class="header">
+
+      <div class="logo">
+        <a href="/">
+          <img src="../../public/logo.png" alt="Logo">
+        </a>
+      </div>
+        
+      <div class="navigation">
+
+        <input type="checkbox" class="toggle-menu">
+        <div class="hamburger"></div>
+
+        <ul class="menu">
           <li>
-            <router-link to="/">Accueil</router-link>
+            <router-link to="/" class="link">Accueil</router-link>
           </li>
           <li>
-            <router-link to="/shop">Le Shop</router-link>
+            <router-link to="/shop" class="link">Le Shop</router-link>
           </li>
           <li>
-            <router-link to="/flash">Flash</router-link>
+            <router-link to="/flash" class="link">Flash</router-link>
           </li>
           <li>
-            <router-link to="/artistes">Artistes</router-link>
+            <router-link to="/artistes" class="link">Artistes</router-link>
           </li>
           <li>
-            <router-link to="/contact">Contact</router-link>
+            <router-link to="/contact" class="link">Contact</router-link>
           </li>
           <li>
-            <router-link to="/faq">FAQ</router-link>
+            <router-link to="/faq" class="link">FAQ</router-link>
           </li>
         </ul>
-    </nav>
+
+      </div>
+        
+      </header>
   </template>
   
   <script>
@@ -29,4 +44,142 @@
     name: "NavbarComponent",
   };
   </script>
+
+<style scoped>
+.header{
+  position: relative;
+  width: 100%;
+  height: 6.25rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--bronze-30);
+  padding: 0 4vw;
+  transition: 0.3s;
+}
+.logo a{
+  display: block;
+}
+.logo a img{
+  display: block;
+  width: 100%;
+}
+.navigation{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.menu{
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  z-index: 1;
+  transition: 0.5s;
+}
+.menu li{
+  list-style-type: none;
+}
+.menu li .link{
+  color: var(--light-grey);
+  text-decoration: none;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1.25rem;
+  font-weight: 400;
+  padding: 40px 25px;
+  line-height: 1;
+  transition: 0.3s;
+}
+.hamburger{
+  position: relative;
+  width: 30px;
+  height: 4px;
+  background-color: var(--light-grey);
+  border-radius: 10px;
+  z-index: 2;
+  transition: 0.3s;
+}
+.hamburger:before, .hamburger:after{
+  content: '';
+  position: absolute;
+  height: 4px;
+  width: 30px;
+  right: 0;
+  background: var(--light-grey);
+  border-radius: 10px;
+  transition: 0.3s;
+}
+.hamburger:before{
+  top: -10px;
+}
+.hamburger:after{
+  top: 10px;
+}
+.toggle-menu{
+  position: absolute;
+  width: 30px;
+  height: 100%;
+  z-index: 3;
+  opacity: 0;
+}
+.hamburger, .toggle-menu{
+  display: none;
+}
+.navigation input:checked ~ .hamburger{
+  background: transparent;
+}
+.navigation input:checked ~ .hamburger:before{
+  top: 0;
+  transform: rotate(-45deg);
+}
+.navigation input:checked ~ .hamburger:after{
+  top: 0;
+  transform: rotate(45deg);
+}
+.navigation input:checked ~.menu{
+  right: 0;
+}
+
+/* Responsive */
+@media (max-width : 480px) {
+  .header{
+    height: 4.375rem;
+    padding: 10px 20px;
+  }
+  .logo a img{
+    width: 40px;
+    height: 40px;
+  }
+  .hamburger, .toggle-menu{
+    display: block;
+  }
+  .menu{
+    justify-content: space-evenly;
+    flex-direction: column;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    right: -100%;
+    background: var(--black);
+    width: 100%;
+    height: 70%;
+    padding-top: 5rem;
+  }
+  .menu li{
+    width: 100%;
+  }
+  .menu li .link{
+    padding: 30px;
+    font-size: 1.5rem;
+  }
+}
+
+@media (min-width: 481px) and (max-width : 1024px) {
+  .menu li .link{
+    font-size: 1.125rem;
+    padding: 30px 15px;
+  }
+}
+
+</style>
    
