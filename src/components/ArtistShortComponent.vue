b95b827077228bca909d4c64e2555a8a3fc6fb56
Enora Lafforgue <enora.lafforgue@gmail.com>
2023-07-12 14:59:19 +0200

    Update HomeView and create ArtistShortComponent

---
+++ src/components/ArtistShortComponent.vue
@@ -0,0 +1,93 @@
+<!-- src/components/ArtistShortComponent.vue -->
+<template>
+    <section class="artist">
+        <div class="container">
+            <div class="img" :style="{ 'background-image': 'url(' + profilePicture + ')' }"></div>
+            <p class="name">{{ name }}</p>
+            <button class="portfolio-button">Portfolio</button>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+name: "ArtistShortComponent",
+props: {
+    profilePicture : {
+        type : String,
+        required : true
+    },
+    name : {
+        type : String,
+        required : true
+    }
+}
+};
+</script>
+
+<style scoped>
+.artist{
+    display: flex;
+    flex-direction: column;
+}
+.img, .name, .portfolio-button{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+}
+.img{
+    width: 300px;
+    height: 400px;
+    border-radius: 20px;
+    border: 2px solid var(--logo-color);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+.name{
+    font-family: 'Noto Serif';
+    font-size: 1.5rem;
+    font-weight: 600;
+    margin-top: 1.56rem;
+}
+.portfolio-button{
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.25rem;
+    font-weight: 400;
+    margin-top: 1.44rem;
+    margin-left: auto;
+    margin-right: auto;
+    color: var(--black);
+    background-color: var(--bronze);
+    width: 150px;
+    height: 30px;
+    border-radius: 10px;
+    border: none;
+    outline: none;
+}
+
+
+/* Responsive */
+@media (max-width : 480px) {
+    .img{
+        width: 200px;
+        height: 200px;
+    }
+    .name{
+        font-size: 1.125rem;
+    }
+    .portfolio-button{
+        font-size: 1rem;
+        width: 100px;
+    }
+}
+
+@media (min-width: 481px) and (max-width : 1024px) {
+    .img{
+        width: 250px;
+        height: 300px;
+    }
+}
+
+</style>
+   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,122 @@
 <script setup>
-
+  import Artist from "../components/ArtistShortComponent.vue";
 </script>
 
 <template>
+
   <h1>Home Page</h1>
+
+  <div class="artists">
+
+    <Artist profilePicture="/david.jpg" name="David"></Artist>
+    <Artist id="artist-2" profilePicture="/veronique.jpg" name="Véronique"></Artist>
+
+  </div>
+  <div class="artists-mobile">
+
+    <div class="artist-1" v-if="artist">
+      <svg id="arrow-left" xmlns="http://www.w3.org/2000/svg" width="19" height="26" viewBox="0 0 19 26" fill="none">
+      <path d="M17.5 25L2 13.4L17.5 1" stroke="#14110E" stroke-width="2" stroke-linecap="round"/>
+      </svg>
+      <Artist class="artist" profilePicture="/david.jpg" name="David"></Artist>
+      <svg id="arrow-right" xmlns="http://www.w3.org/2000/svg" width="19" height="26" viewBox="0 0 19 26" fill="none" v-if="artist" @click="showNextArtist">
+      <path d="M1 1L16.5 12.6L1 25" stroke="#EFEFF1" stroke-width="2" stroke-linecap="round"/>
+      </svg>
+    </div>
+
+    <div class="artist-2" v-if="!artist">
+      <svg id="arrow-left" xmlns="http://www.w3.org/2000/svg" width="19" height="26" viewBox="0 0 19 26" fill="none" v-if="!artist" @click="showPreviousArtist">
+      <path d="M17.5 25L2 13.4L17.5 1" stroke="#EFEFF1" stroke-width="2" stroke-linecap="round"/>
+      </svg>
+      <Artist class="artist" profilePicture="/veronique.jpg" name="Véronique"></Artist>
+      <svg id="arrow-right" xmlns="http://www.w3.org/2000/svg" width="19" height="26" viewBox="0 0 19 26" fill="none">
+      <path d="M1 1L16.5 12.6L1 25" stroke="#14110E" stroke-width="2" stroke-linecap="round"/>
+      </svg>
+    </div>
+    
+  </div>
+
+  <!-- 
+  <div class="artists" v-for="artist in artists" :key="artist.name">
+    <Artist :profilePicture="artist.profilePicture" :name="artist.name"></Artist>
+  </div>
+  --> 
+
 </template>
 
 <script>
+  export default {
+    name: "HomeView",
+    components: {
+      Artist,
+    },
+    data() {
+      return {
+        artist : true,
+        artists: [
+          {
+            name: 'David',
+            profilePicture: '/david.jpg'
+          },
+          {
+            name: 'Véronique',
+            profilePicture: '/veronique.jpg'
+          }
+        ]
+      }
 
-
-export default {
-  name: "HomeView",
-  components: {
-    
+    },
+    methods: {
+      showNextArtist() {
+        this.artist = false;
+      },
+      showPreviousArtist() {
+        this.artist = true;
+      },
+    }
   }
-}
 </script>
 
 <style scoped>
+.artists{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+#artist-2{
+  margin-left: 7rem;
+}
+.artists-mobile{
+  display: none;
+}
 
+/* Responsive */
+@media (max-width : 480px) {
+  .artists{
+    display: none;
+  }
+  .artists-mobile{
+    display: flex;
+  }
+  .artist-1, .artist-2{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  #arrow-right{
+    margin-left: 2.3rem;
+    margin-top: 5.5rem;
+  }
+  #arrow-left{
+    margin-right: 2.3rem;
+    margin-top: 5.5rem;
+  }
+
+}
+
+@media (min-width: 481px) and (max-width : 1024px) {
+
+}
 </style>
